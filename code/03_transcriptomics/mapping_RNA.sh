48f22c06886c7964bfd72924e5e028573169aec6
Alexander Renlund <arenlund1@gmail.com>
2025-05-23 09:18:08 +0200

    Last versions of transcriptomic analysis - changed mapping and feature counting to the polished genome and annotated file of strain R7

--- code/03_transcriptomics/mapping_RNA.sh
+++ code/03_transcriptomics/mapping_RNA.sh
@@ -18,30 +18,25 @@ TRIMMED_RNA="/home/alre1394/GA_PROJECT_PAPER2/data/01_processing/trimmed/RNA"
 ASSEMBLIES="/home/alre1394/GA_PROJECT_PAPER2/data/02_genomics/assemblies"
 OUTPUT="/home/alre1394/GA_PROJECT_PAPER2/data/03_transcriptomics/mapped_RNA"
 
-#CONSTRUCTING FM-INDEX FOR FLIPPED R7 POLISHED GENOME
+CONSTRUCTING FM-INDEX FOR R7 FLIPPED POLISHED GENOME
 bwa index "$ASSEMBLIES/R7/polished/R7_genome_polished_flipped.fasta"
-#GENOME MAPPING THE TRIMMED RNA PAIRED-END READS FROM STRAIN R7 TO ITS POLISHED AND FLIPPED GENOME 
-for code in  SRR24516462 SRR24516463 SRR24516464
+#GENOME MAPPING THE TRIMMED RNA PAIRED-END READS FROM STRAIN R7 TO ITS POLISHED AND FLIPPED GENOME
+for code in SRR24516462 SRR24516463 SRR24516464
 do 
-bwa mem -t 8 "$ASSEMBLIES/R7/polished/R7_genome_polished_flipped.fasta" "$TRIMMED_RNA/${code}_forward_paired.fq.gz" "$TRIMMED_RNA/${code}_reverse_paired.fq.gz" | samtools view -Sb - | samtools sort -o "$OUTPUT/R7_flipped_${code}_sorted.bam"
-samtools index "$OUTPUT/R7_flipped_${code}_sorted.bam"
+bwa mem -t 8 "$ASSEMBLIES/R7/polished/R7_genome_polished_flipped.fasta" "$TRIMMED_RNA/${code}_forward_paired.fq.gz" "$TRIMMED_RNA/${code}_reverse_paired.fq.gz" | samtools view -Sb -@ 8 | samtools sort -@ 8 -o "$OUTPUT/R7_flipped_${code}_sorted.bam"
+samtools index --threads 8 "$OUTPUT/R7_flipped_${code}_sorted.bam"
 done
 
-
-#CONSTRUCTING FM-INDEX FOR HP126 POLISHED GENOME
-bwa index "$ASSEMBLIES/HP126/polished/HP126_genome_polished.fasta"
-#GENOME MAPPING THE TRIMMED RNA PAIRED-END READS FROM STRAIN HP126 TO ITS POLISHED GENOME
+#GENOME MAPPING THE TRIMMED RNA PAIRED-END READS FROM STRAIN HP126 TO THE REFERENCE GENOME OF R7
 for code in SRR24516459 SRR24516460 SRR24516461
 do 
-bwa mem -t 8 "$ASSEMBLIES/HP126/polished/HP126_genome_polished.fasta" "$TRIMMED_RNA/${code}_forward_paired.fq.gz" "$TRIMMED_RNA/${code}_reverse_paired.fq.gz" | samtools view -Sb - | samtools sort -o "$OUTPUT/HP126_${code}_sorted.bam"
-samtools index "$OUTPUT/HP126_${code}_sorted.bam"
+bwa mem -t 8 "$ASSEMBLIES/R7/polished/R7_genome_polished_flipped.fasta" "$TRIMMED_RNA/${code}_forward_paired.fq.gz" "$TRIMMED_RNA/${code}_reverse_paired.fq.gz" | samtools view -Sb -@ 8 | samtools sort -@ 8 -o "$OUTPUT/HP126_${code}_sorted.bam"
+samtools index --threads 8 "$OUTPUT/HP126_${code}_sorted.bam"
 done
 
-#CONSTRUCTING FM-INDEX FOR FLIPPED DV3 POLISHED GENOME
-bwa index "$ASSEMBLIES/DV3/polished/DV3_genome_polished_flipped.fasta"
-#GENOME MAPPING THE TRIMMED RNA PAIRED-END READS FROM STRAIN DV3 TO ITS POLISHED AND FLIPPED GENOME
+#GENOME MAPPING THE TRIMMED RNA PAIRED-END READS FROM STRAIN DV3 TO THE REFERENCE GENOME OF R7
 for code in SRR24516456 SRR24516457 SRR24516458
 do
-bwa mem -t 8 "$ASSEMBLIES/DV3/polished/DV3_genome_polished_flipped.fasta" "$TRIMMED_RNA/${code}_forward_paired.fq.gz" "$TRIMMED_RNA/${code}_reverse_paired.fq.gz" | samtools view -Sb - | samtools sort -o "$OUTPUT/DV3_flipped_${code}_sorted.bam"
-samtools index "$OUTPUT/DV3_flipped_${code}_sorted.bam"
+bwa mem -t 8 "$ASSEMBLIES/R7/polished/R7_genome_polished_flipped.fasta" "$TRIMMED_RNA/${code}_forward_paired.fq.gz" "$TRIMMED_RNA/${code}_reverse_paired.fq.gz" | samtools view -Sb -@ 8 | samtools sort -@ 8 -o "$OUTPUT/DV3_flipped_${code}_sorted.bam"
+samtools index --threads 8 "$OUTPUT/DV3_flipped_${code}_sorted.bam"
 done
--- code/03_transcriptomics/read_count.sh
+++ code/03_transcriptomics/read_count.sh
@@ -3,7 +3,7 @@
 #SBATCH -A uppmax2025-3-3
 #SBATCH -M snowy
 #SBATCH -p core
-#SBATCH -n 6
+#SBATCH -n 8
 #SBATCH -t 06:00:00
 #SBATCH -J GA_read_counting_alre1394
 #SBATCH --mail-type=ALL
@@ -17,8 +17,8 @@ export ASSEMBLIES="/home/alre1394/GA_PROJECT_PAPER2/data/02_genomics/assemblies"
 export MAPPED_RNA="/home/alre1394/GA_PROJECT_PAPER2/data/03_transcriptomics/mapped_RNA"
 export OUTPUT="/home/alre1394/GA_PROJECT_PAPER2/data/03_transcriptomics/read_counts"
 
-#COUNTING MAPPED RNA READS OF GENOMICS FEATURES
-#FOR R7 FLIPPED GENOME
+
+#COUNTING READS FROM MAPPED RNA FEATURES OF EACH SAMPLE FROM R7
 for code in SRR24516462 SRR24516463 SRR24516464
 do
 htseq-count --mode union -f bam -t CDS -r pos -i ID \
@@ -26,18 +26,18 @@ htseq-count --mode union -f bam -t CDS -r pos -i ID \
             "$ASSEMBLIES/R7/annotation/flipped/R7_annotated_del.gff" > "$OUTPUT/R7/${code}_counts.txt"
 done
 
-#FOR DV3 FLIPPED GENOME
-for code in SRR24516456 SRR24516457 SRR24516458
-do
-htseq-count --mode union -f bam -t CDS -r pos -i ID \
-            "$MAPPED_RNA/DV3_flipped_${code}_sorted.bam" \
-            "$ASSEMBLIES/DV3/annotation/flipped/DV3_annotated_del.gff" > "$OUTPUT/DV3/${code}_counts.txt"
-done
-
-#FOR HP126 UNFLIPPED GENOME
+#COUNTING READS FROM MAPPED RNA FEATURES OF EACH SAMPLE FROM HP126, with R7 ANNOTATION AS REFERENCE
 for code in SRR24516459 SRR24516460 SRR24516461
 do
 htseq-count --mode union -f bam -t CDS -r pos -i ID\
             "$MAPPED_RNA/HP126_${code}_sorted.bam" \
-            "$ASSEMBLIES/HP126/annotation/HP126_annotated_del.gff" > "$OUTPUT/HP126/${code}_counts.txt"
+            "$ASSEMBLIES/R7/annotation/flipped/R7_annotated_del.gff" > "$OUTPUT/HP126/${code}_counts.txt"
+done
+
+#COUNTING READS FROM MAPPED RNA FEATURES OF EACH SAMPLE FROM DV3, with R7 ANNOTATION AS REFERENCE
+for code in SRR24516456 SRR24516457 SRR24516458
+do
+htseq-count --mode union -f bam -t CDS -r pos -i ID \
+            "$MAPPED_RNA/DV3_flipped_${code}_sorted.bam" \
+            "$ASSEMBLIES/R7/annotation/flipped/R7_annotated_del.gff" > "$OUTPUT/DV3/${code}_counts.txt"
 done
